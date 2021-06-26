3262a3c63e9a75d310c41713f9383c191b556a7c
Halil İbrahim Bestil <hibestil@gmail.com>
2021-06-26 18:20:50 +0300

    Created job class

---
+++ deliver/job.py
@@ -0,0 +1,11 @@
+class Job:
+    job_id = None  # Order id
+    location_index = None  # Index of the order location
+    delivery = None  # The amount of carboy that will be delivered in this job
+    service = None  # Service duration, in seconds
+
+    def __init__(self, job_id, location_index, delivery, service):
+        self.job_id = job_id
+        self.location_index = location_index
+        self.delivery = delivery
+        self.service = service
