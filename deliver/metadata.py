97cb01c09dbec8a58b11737005fe59b63c644549
Halil İbrahim Bestil <hibestil@gmail.com>
2021-06-26 17:24:43 +0300

    Initialized project structure

---
+++ deliver/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'deliver'
+project = "Path optimization service"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = '-'
+authors = ['Halil Ibrahim Bestil']
+authors_string = ', '.join(authors)
+emails = ['hibestil@gmail.com']
+license = 'MIT'
+copyright = '2021 ' + authors_string
+url = 'http://bestil.in/'
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+Path optimization service
+=========================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+.. only:: html
+
+   Indices and tables
+   ==================
+
+   * :ref:`genindex`
+   * :ref:`modindex`
+   * :ref:`search`
