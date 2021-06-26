134049e4a44b677e05157f88316ede9377ff28f5
Halil İbrahim Bestil <hibestil@gmail.com>
2021-06-26 18:20:38 +0300

    Added comments

--- deliver/vehicle.py
+++ deliver/vehicle.py
@@ -1,7 +1,7 @@
 class Vehicle:
-    vehicle_id = None
-    start_index = None
-    capacity = 0
+    vehicle_id = None  # Vehicle id
+    start_index = None  # Index of the vehicle’s starting location
+    capacity = 0  # Initial carboy capacity of the vehicle
 
     def __init__(self, vehicle_id, start_index, capacity):
         self.vehicle_id = vehicle_id
