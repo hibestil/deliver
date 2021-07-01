72c716dffaaaf3dfcac8355e2995fd562a58c8bf
Halil İbrahim Bestil <hibestil@gmail.com>
2021-07-02 02:48:38 +0300

    Implemented find closest depot feature

--- deliver/genetic_solver.py
+++ deliver/genetic_solver.py
@@ -2,7 +2,23 @@ from deliver.problem_solver import ProblemSolver
 
 
 class GeneticSolver(ProblemSolver):
+    groups = None
+
+    def __init__(self, problem):
+        super().__init__(problem)
+        self.groups = [[] for i in range(len(self.problem.depots))]
 
     def solve(self):
-        [print(v) for v in self.vehicles]
         return
+    def find_closest_depot(self, customer_id):
+        closest_depot = None
+        closest_distance = -1
+        for i, depot in enumerate(self.problem.depots):
+            from_c_to_d = self.problem.matrix.data[i][customer_id]
+            from_d_to_c = self.problem.matrix.data[customer_id][i]
+            total_distance = from_c_to_d + from_d_to_c
+            if closest_depot is None or total_distance < closest_distance:
+                closest_depot = (depot, i)
+                closest_distance = total_distance
+
+        return closest_depot[0], closest_depot[1], closest_distance
