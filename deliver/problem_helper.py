5dd91ad02b9c05e16c88aeabc986f84661098262
Halil İbrahim Bestil <hibestil@gmail.com>
2021-07-03 05:07:40 +0300

    Set max vehicle number for depots.

--- deliver/problem_helper.py
+++ deliver/problem_helper.py
@@ -39,9 +39,9 @@ class ProblemHelper:
 
     @staticmethod
     def define_depots(vehicles):
-        depot_idxs = [d.start_index for d in vehicles]
+        return [Depot(d.start_index, 2, inf, d.capacity[0]) for d in vehicles]
         # n_vehicles_in_depot = Counter(depot_idxs)
-        return [Depot(d, inf, inf, inf) for d in list(set(depot_idxs))]
+        # return [Depot(d, inf, inf, d) for d in list(set(depot_idxs))]
 
     @staticmethod
     def define_customers(jobs):
