f9305484afb5bea6223aa30e9b106742d4660559
Halil İbrahim Bestil <hibestil@gmail.com>
2021-07-03 16:41:19 +0300

    Updated to_string function of Customer class

--- deliver/customer.py
+++ deliver/customer.py
@@ -6,6 +6,7 @@ class Customer:
         self.location_index = location_index
 
     def __str__(self):
-        return "[Customer] :: id:{} | service_duration:{} | demand:{}".format(self.id,
-                                                                              self.service_duration,
-                                                                              self.demand)
+        return "[Customer] :: id:{} | service_duration:{} | demand:{} | index: {}".format(self.id,
+                                                                                        self.service_duration,
+                                                                                        self.demand,
+                                                                                        self.location_index)
