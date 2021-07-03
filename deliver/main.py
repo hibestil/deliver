e18b1f1a977109689d1fc3be7ce4a73debe2f3d3
Halil İbrahim Bestil <hibestil@gmail.com>
2021-07-03 22:33:44 +0300

    Added comments to main.py

--- deliver/main.py
+++ deliver/main.py
@@ -11,6 +11,7 @@ from deliver.problem.problem_helper import ProblemHelper
 
 
 def main(argv):
+    # Genetic Algorithm Parameters
     generations = 2500
     population_size = 50
     crossover_rate = 0.05
@@ -19,13 +20,15 @@ def main(argv):
     depot_move_mutate_rate = 0
     best_insertion_mutate_rate = 0.1
     route_merge_rate = 0.05
-
+    # Read problem JSON from argv
     json_path = sys.argv[1]
     problem = ProblemHelper(json_path)
-    model = GeneticSolver(problem,population_size=population_size)
+    # Create Genetic Algorithm solver object
+    model = GeneticSolver(problem, population_size=population_size)
+    # Solve the problem
     solution = model.solve(generations, crossover_rate, heuristic_mutate_rate, inversion_mutate_rate,
                            depot_move_mutate_rate, best_insertion_mutate_rate, route_merge_rate)
-
+    # Print out the solution
     model.show(solution)
     return 0
 
